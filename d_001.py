b9ee6a8c6841941cfee63da44f2bc000dabdc0e2
bardo <edgareduardo.chaux@gmail.com>
2024-07-26 13:57:41 -0500

    d_001.py

---
+++ d_001.py
@@ -0,0 +1,15 @@
+'''Este es el primer día de una serie de 100 días aprendiendo python. En el primer día se aprenderán las variables, salida y entrada de datos.'''
+#Teoría
+#Print()
+'''Lo primero que se suele enseñar es cómo imprimir en consola los resultados de los programas que se realizan.
+Para ello, se utiliza la función print()'''
+print ('Hola mundo')
+
+#Variables: 
+
+'''son contenedores de valores que pueden variar su valor. Para asignar variables sólo se debe asignar un valor
+Convención de nombre de variables: en minúsculas, sin acentos y sin caracteres especiales. Para más de una palabra se usa el guión bajo.
+Ejemplo de variables convencionalmente bien declaradas:
+'''
+
+#Práctica
