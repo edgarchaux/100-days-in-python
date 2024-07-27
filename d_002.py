d27f614c9370842f2edb04c44aca6b451d20ec63
bardo <edgareduardo.chaux@gmail.com>
2024-07-27 17:01:49 -0500

    d_002

---
+++ d_002.py
@@ -0,0 +1,10 @@
+#Este es el día 2 de los 100 días en python
+#Tipos de datos y conversiones
+#Strings
+'''Tipo de texto. Para imprimir el tipo print se usan las comillas simples y las dobles.
+por ejemplo print('Este es un "string"')'''
+#len
+'''Es una función que devuelve el número de caracteres de una palabra. Por ejemplo:
+print(len('amarillo')) devuelve 8. La función len recibe strings o variables. la opción len(amarillo[2]) devuelve a, ya que es la posición en el string contando a partir de 0'''
+#int
+''''''
