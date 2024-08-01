6d3935b17cab1fbf981319bc2e750d0922b31882
bardo <edgareduardo.chaux@gmail.com>
2024-07-31 22:01:45 -0500

    teoria

--- d_005.py
+++ d_005.py
@@ -1,2 +1,43 @@
 #Bucles for y while
 #Parte teórica
+#Bucles
+'''Estructuras de flujo que permiten hacer varias repeticiones automáticamente
+Flujo de ejecución
+va verticalmente desde la línea 1 hasta la última, en los bucles sino se cumple su condición se ignora su código,
+sino se cumple su ejecución se repite hasta que se cumpla.
+Bucle FOR
+Si se cumple la condición, ignora el bucle, sino, se ejecuta
+for i in range(5):
+    print(f"El valor del bucle es {i}")
+Si se añade otro argumento, el primero da el valor inicial
+for i in range(3,7):
+    print(f"El valor del bucle es {i}")
+print("Fin del bucle").
+Si se añade un tercer argumento, indica el incremento
+for i in range (3,12,3):
+    print(f"El valor del bucle es {i}")
+print ("Fin del bucle")
+Si el tercer argumento es negativo, indica deccrecimiento
+for i in range (3,12,-3):
+    print(f"El valor del bucle es {i}")
+print ("Fin del bucle")
+Iterar listas
+Para realizar una acción muchas veces
+colores=["rojo","azul","verde","amarillo"]
+print("---LISTADO DE COLORES---")
+for color in colores
+    print(f"-{color}-")
+El bucle ahorra tiempo y recursos
+condicionales dentro de bucles
+permiten terminar los bucles antes de ejecutrlo todo, por ejemplo:
+for color in colores:
+    if color=="azul"":
+        print(f"Se ha roto la ejecución del bucle")
+        break
+    print(f"-color {color}.")
+Bucle while
+i=1
+while i<5:
+    print(f"El valor del bucle es: {i}")
+    i+=1
+'''
