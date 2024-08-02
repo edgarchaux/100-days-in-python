fbab571c537496e569f3ed56af2175539051c8eb
bardo <edgareduardo.chaux@gmail.com>
2024-08-02 00:04:57 -0500

    practica

--- d_006.py
+++ d_006.py
@@ -25,4 +25,23 @@ Argumentos de clave
 La ventaja que tienen es que se pueden cambiar el orden en la declaración
 saludar(nombre="Quique", edad=30)
 return
+Devuelve un resultado para utilizarlo dentro de la función
+def suma(numero1,numero2):
+    print(numero1+numero2)
+suma(10,50)
+muestra el resultado en la consola, pero si se necesita el resultado para hacer algo, no se puede
+scope
+significa que no se pueden usar variables definidas dentro de una función fuera de la misma
+def suma(numero1,numero2):
+    return numero1 + numero 2
+resultado=suma(10,50)
+de esta forma se guarda el valor del resultado de la función suma()
 '''
+#practica
+'''
+1.  añadir un color al principio de la lista con el uso de una función'''
+colores=["rojo","verde","amarillo"]
+def color(color):
+    colores.insert(0,color)
+color(str(input("Escriba un color para añadirlo a la lista: \n")))
+print(colores)
