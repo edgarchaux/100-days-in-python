9666e81a9ea447ccfe979c42be4e739978a0ac47
bardo <edgareduardo.chaux@gmail.com>
2024-08-04 12:20:24 -0500

    parte teorica

--- d_010.py
+++ d_010.py
@@ -1,3 +1,21 @@
 #Funciones lambda
 #Parte teórica
-''''''
+'''
+Son funciones cortas con sintaxis reducida, solo tiene una expresión
+Por ejemplo:
+def multiplicacion(num1, num2):
+    return num1*num2
+en funcion lambda sería:
+lambda num1, num2: num1*num2
+Para llamar a las funciones lambda:
+se almacenan en variables, por ejemplo:
+multiplicacion=lambda num1, num2: num1*num2
+resultado1=multiplicacion(10,7)
+print(resultado1)
+Otra forma de llamarlas es:
+(lambda num1, num2: print(num1*num2))(7,5)
+Lambda no se puede hacer si se requiere más de una expresión
+'''
+#Parte práctica
+'''
+1.  '''
