2e86db86058b4e75a1454d6dc235f674ce3c696d
bardo <edgareduardo.chaux@gmail.com>
2024-08-03 20:57:50 -0500

    teoria

--- d_009.py
+++ d_009.py
@@ -1,1 +1,12 @@
 #Tkinter- personalizar ventana-pack y grid
+#Parte teórica
+'''
+Importaciones
+from tkinter import *
+creación de la ventana principal:
+root=Tk()
+Título de la ventana
+root.title("Curso de TKinter")
+Tamaño de la ventana
+root.geometry("800x600")
+'''
