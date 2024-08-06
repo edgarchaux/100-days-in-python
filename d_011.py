c8e7838b0ee6a7fb099c4041e180f49b5621cb49
bardo <edgareduardo.chaux@gmail.com>
2024-08-05 21:42:04 -0500

    teoria

--- d_011.py
+++ d_011.py
@@ -1,2 +1,29 @@
 #POO
 #Parte teórica
+'''Poo se refiere a un paradigma que representa objetos reales o imaginarios
+cuyas particularidades se llaman atributos, así:
+Taza_1
+color="blanco"
+mensaje=none
+material="porcelana"
+limpia=True
+Taza_2
+clases
+sistemas de generadores de objetos
+sintaxis
+class Taza():
+    color="blanco"
+    mensaje=none
+    material="porcelana"
+    limpia=True"
+Instancias del objeto
+cuando se generan a partir de las clases
+¿Cómo instanciar un objeto:
+taza_1=Taza()
+taza_2=Taza()
+para ver la dirección de memoria
+print(taza_1)
+print(taza_2)
+¿Cómo se acceden a los atributos de un objeto?
+print(taza_1.color)
+'''
