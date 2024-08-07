9ede6e4629524cf9bdcde610581bd47d42d77c4e
bardo <edgareduardo.chaux@gmail.com>
2024-08-06 21:49:59 -0500

    project

--- d_011.py
+++ d_011.py
@@ -71,7 +71,8 @@ class  Motocicleta():
     estado="nueva"
     motor=False
     #Métodos
-    def __init__(self, color, matricula, combustible_litros, numero_ruedas, marca, modelo, fecha_fabricacion, velocidad_punta, peso):
+    def __init__(self, color, matricula, combustible_litros, numero_ruedas, marca, modelo, fecha_fabricacion, velocidad_punta, peso, tope):
+        self.tope=tope
         self.color=color
         self.matricula=matricula
         self.combustible_litros=combustible_litros
@@ -94,6 +95,21 @@ class  Motocicleta():
             self.motor=False
         else:
             print("El motor ya estaba detenido")
+    def mostrar_precio(self):
+        print(f"El precio de la motocicleta {self.marca}{self.modelo}es de {self.precio}")
+    def comprobar_carga(self):
+        print(f"Bienvenido a su gasolinera\nLa motocicleta {self.marca} de modelo {self.modelo} con matrícula {self.matricula}\nTiene {self.tope} de capacidad y una carga actual de {self.combustible_litros} litros\nPor tanto le quedan {self.tope-self.combustible_litros} litros por llenar\n Fin del reporte")
+    def repostar(self):
+        while True:
+            carga=int(input("Por favor ingrese los litros que desea cargar\n"))
+            if carga>self.tope-self.combustible_litros:
+                print("La carga supera el tope de gasolina de la moto, vuelve a intentarlo")
+            elif self.tope==self.combustible_litros:
+                print("Carga completa")
+            else:
+                print(f"Moto reposteada, la moto tiene {carga+self.combustible_litros} de carga")
+                self.combustible_litros+=carga
+                break
 '''
 2.  Añadir un atributo de clase especificando que todas las motos son nuevas'''
 '''
@@ -109,10 +125,10 @@ Para el método detener(): si el motor está en marcha, se indica que el motor s
 opcionalmente, que el método haga cambiar el estado del motor al llamar a los métodos'''
 '''
 7.  Instanciar una motocicleta con los siguientes valores:combustible_litros=10 y numero_ruedas=2'''
-moto_1=Motocicleta("","",10,2,"","","","","")
+moto_1=Motocicleta("","ysa-848",10,2,"Honda","2020","","","",17)
 '''
 8.  Crear otra instancia de motocicleta utilizando los argumentos de clave, en lugar de los posicionales, en depósito colocar vacío'''
-moto_2=Motocicleta(matricula="asd-123",color="gris",combustible_litros="vacío", numero_ruedas=2, marca="Suzuki",modelo="2019",fecha_fabricacion="",velocidad_punta="220mi/h",peso="35kls")
+moto_2=Motocicleta(matricula="asd-123",color="gris",combustible_litros="vacío", numero_ruedas=2, marca="Suzuki",modelo="2019",fecha_fabricacion="",velocidad_punta="220mi/h",peso="35kls",tope=20)
 '''
 9.  Probar los métodos de arranque y detener con una de los dos objetos'''
 moto_1.arrancar()
@@ -128,3 +144,20 @@ moto_1.precio=19000
 print(f"El precio de la motocicleta {moto_1.marca}{moto_1.modelo}es de {moto_1.precio}")
 '''
 12. Hacer la misma consulta desde la clase'''
+moto_1.mostrar_precio()
+'''
+13. Llamar al nuevo método con los 2 objetos'''
+#moto_2.mostrar_precio()
+'''
+14. Crear un método capaz de repostar las motos
+crear un método para comprobar la cantidad de combustible que tienen las motos
+se deberá indicar la cantidad de combustible que tiene, la capacidad máxima del tanque de combustible y los litros que faltan para llenar el depósito
+La salida de este método debe ser un reporte. Se añade todo lo anterior con un título personalizado con el nombre de la moto que consulta
+Establecer un tope de depósito que se indica con un nuevo atributo
+Crear un método que se utilice para poner la cantidad de litros que se quieren repostar mediante un input
+Si la cantidad de litros es superior a la de la capacidad que hay en el depósito se deberá advertir de que no se puede repostar esa cantidad y se le dejará intentarlo de nuevo las veces que haga falta
+Si el repostaje es correcto, se indicará en consola la cantidad de litros que tiene la moto
+Mostrar el cambio
+'''
+moto_1.comprobar_carga()
+moto_1.repostar()
