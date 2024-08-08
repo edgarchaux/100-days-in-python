696ceb48a6542fa1500c296e8d68295499b0d029
bardo <edgareduardo.chaux@gmail.com>
2024-08-07 21:43:32 -0500

    teoria

--- d_012.py
+++ d_012.py
@@ -11,13 +11,17 @@ crear ventana principal
 root=Tk()
 Título de la ventana
 root.title("Curso de Tkinter de programación fácil")
+entrada de datos
 entrada=Entry(root)
+entrada.insert(0, "Escriba su nombre")
+entrada.bind("<key>", lambda e:entrada.delete(0,END))
+entrada.insert(10,"pf")
 entrada.pack()
 Evento para la función
 def pulsar_boton():
-    texto=entrada.get()
-    Label(root, text=f"{texto}").pack()
+    nombre=entrada.get()
+    Label(root, text=f"{nombre}").pack()
 Botón
-boton_1=Button(root, text="pulsa aquí", command=pulsar_boton).pack()
+boton_1=Button(root, text="enviar", command=pulsar_boton).pack()
 Bucle de ejecución
 root.mainloop()'''
