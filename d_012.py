967005f5a7beac959129bc3becc4669f8c36ab5f
bardo <edgareduardo.chaux@gmail.com>
2024-08-07 21:25:11 -0500

    a

--- d_012.py
+++ d_012.py
@@ -1,1 +1,23 @@
 #Iniciación a los eventos con TKinter
+#Parte teórica
+'''
+Eventos con mouse y teclado
+Se trata de que se ejecute una acción cuando se ejecute un activador
+GUI
+son los programas de interfaz gráfica
+Importaciones
+from tkinter import *
+crear ventana principal
+root=Tk()
+Título de la ventana
+root.title("Curso de Tkinter de programación fácil")
+entrada=Entry(root)
+entrada.pack()
+Evento para la función
+def pulsar_boton():
+    texto=entrada.get()
+    Label(root, text=f"{texto}").pack()
+Botón
+boton_1=Button(root, text="pulsa aquí", command=pulsar_boton).pack()
+Bucle de ejecución
+root.mainloop()'''
