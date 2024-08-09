09cdc4631735d09cd2e36a4570a353a1618164da
bardo <edgareduardo.chaux@gmail.com>
2024-08-08 22:34:02 -0500

    project

--- d_013.py
+++ d_013.py
@@ -47,3 +47,19 @@ num_1=10
 num_2=34.50
 result=num_1*num_2
 print("La multiplicación de %i * %f da como resultado: %f."%(num_1,num_2,result))
+'''
+4.  Crear un sistema que sepa contar las coincidencias de una letra introducida en la consola tiene un texto'''
+phrase="aleluya"
+suma=0
+letra=input("Buscar letra\n")
+for l in range (len(phrase)):
+    if(letra==phrase[l]):
+        suma+=1
+    else:
+        continue
+print(f"La letra {letra} se repitió {suma} veces")
+'''
+5.  imprimir con 3 decimales'''
+num_1=34.5000000
+num_2=345.000000
+print(("%.3f %.3f")%(num_1,num_2))
